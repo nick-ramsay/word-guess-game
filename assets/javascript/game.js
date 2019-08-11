a27bcf403aa54508d247dbec4c3c30c5b5b2a6fc
Nick Ramsay <NickRamsay1989@gmail.com>
2019-08-11 15:47:30 +1000

    adding latest javascript revisions

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+var wordChoices = {
+    verb:["v","e","r","b"],
+    noun:["n","o","u","n"],
+    vowel:["v","o","w","e","l"]
+}
+
+console.log(wordChoices.vowel); //Returns property value
+console.log(wordChoices.vowel.length); //Finds length of property's value
+console.log(Object.keys(wordChoices)[0]); //Returns the first property in the object
+console.log(Object.keys(wordChoices).length); //Returns count of properties in object
+
+var userScore = 0;
+var wrongGuesses = 0;
+var usedLetters = [];
+
+var chosenWordNumber = Math.floor(Math.random() * Object.keys(wordChoices).length); //Generates a random integer equivalent less than or equal to count of objects in array
+console.log(chosenWordNumber);
+
+var chosenWord = wordChoices.(Object.keys(wordChoices)[chosenWordNumber]); //Uses chosenWord integer to pick value of random word property from word choices object
+console.log(chosenWord);
+
+
+
+
+
+
