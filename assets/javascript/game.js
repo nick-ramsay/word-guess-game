4ea77e737e244945d96e11c3d8450ea8da472873
Nick Ramsay <NickRamsay1989@gmail.com>
2019-08-12 19:35:40 +1000

    consolidated word reset to function and added for loop for multi letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,32 @@
 var wordChoices = {
-    verb: ["v", "e", "r", "b"],
-    noun: ["n", "o", "u", "n"], //Problem with noun must be fixed... indexOf only catches the first occurrence of the letter. It breaks after that... need a for loop to replace all occurrences of letters
-    vowel: ["v", "o", "w", "e", "l"]
+    austrailia: ["c", "a", "n", "b", "e", "r", "r", "a"],
+    unitedStates: ["w", "a", "s", "h", "i", "n", "g", "t", "o", "n"], //Problem with noun must be fixed... indexOf only catches the first occurrence of the letter. It breaks after that... need a for loop to replace all occurrences of letters
+    russia: ["m", "o", "s", "c", "o","w"],
+    china: ["b","e","i","j","i","n","g"],
+    japan: ["t","o","k","y","o"],
+    canada: ["o","t","t","a","w","a"],
+    france: ["p","a","r","i","s"],
+    cuba: ["h","a","v","a","n","a"]
 }
-
-console.log(wordChoices.vowel); //Returns property value
-console.log(wordChoices.vowel.length); //Finds length of property's value
-console.log(Object.keys(wordChoices)[0]); //Returns the first property in the object
-console.log(Object.keys(wordChoices).length); //Returns count of properties in object
 
 var winCount = 0;
 var guessesRemaining = 15;
-var guessedLetters = [];
-console.log(guessedLetters);
 
-var chosenWordNumber = Math.floor(Math.random() * Object.keys(wordChoices).length); //Generates a random integer equivalent less than or equal to count of properties in the object
-console.log(chosenWordNumber);
+var guessedLetters;
 
-var chosenWordProperty = Object.keys(wordChoices)[chosenWordNumber]; //Uses chosenWordNumber to pick a property from object
-console.log(chosenWordProperty);
+var chosenWordNumber; //Generates a random integer equivalent less than or equal to count of properties in the object
+var chosenWordProperty;  //Uses chosenWordNumber to pick a property from object
+var chosenWordValue; //uses chosenWordProperty to pick array value from object
 
-var chosenWordValue = wordChoices[chosenWordProperty]; //uses chosenWordProperty to pick array value from object
-console.log(chosenWordValue);
+var currentWord;
 
-currentWord = Array(wordChoices[chosenWordProperty].length).fill("_");
-console.log(currentWord);
+function newWord() {
+    chosenWordNumber = Math.floor(Math.random() * Object.keys(wordChoices).length);
+    chosenWordProperty = Object.keys(wordChoices)[chosenWordNumber];
+    chosenWordValue = wordChoices[chosenWordProperty];
+    currentWord = Array(wordChoices[chosenWordProperty].length).fill("_");
+    guessedLetters = [];
+}
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
@@ -33,10 +35,7 @@ document.onkeyup = function (event) {
     if (chosenWordValue.indexOf(userGuess) === -1) {
         guessesRemaining--;
         if (guessesRemaining === 0) {
-            chosenWordNumber = Math.floor(Math.random() * Object.keys(wordChoices).length);
-            chosenWordProperty = Object.keys(wordChoices)[chosenWordNumber];
-            chosenWordValue = wordChoices[chosenWordProperty];
-            currentWord = Array(wordChoices[chosenWordProperty].length).fill("_");
+            newWord();
             alert("You've lost. Try Again!");
             winCount = 0;
             guessesRemaining = 15;
@@ -45,36 +44,31 @@ document.onkeyup = function (event) {
     }
 
     else if (chosenWordValue.indexOf(userGuess) !== -1) {
-        currentWord[chosenWordValue.indexOf(userGuess)] = userGuess;
+
+        for (i = chosenWordValue.indexOf(userGuess); i <= chosenWordValue.lastIndexOf(userGuess); i++) {
+            currentWord[chosenWordValue.indexOf(userGuess,i)] = userGuess;
+        }
+
+        //currentWord[chosenWordValue.indexOf(userGuess)] = userGuess;
         if (currentWord.indexOf("_") === -1 && winCount < 5) {
-            chosenWordNumber = Math.floor(Math.random() * Object.keys(wordChoices).length);
-            chosenWordProperty = Object.keys(wordChoices)[chosenWordNumber];
-            chosenWordValue = wordChoices[chosenWordProperty];
-            currentWord = Array(wordChoices[chosenWordProperty].length).fill("_");
-            guessedLetters = [];
+            newWord();
             winCount++;
         }
         if (winCount === 5) {
-            chosenWordNumber = Math.floor(Math.random() * Object.keys(wordChoices).length);
-            chosenWordProperty = Object.keys(wordChoices)[chosenWordNumber];
-            chosenWordValue = wordChoices[chosenWordProperty];
-            currentWord = Array(wordChoices[chosenWordProperty].length).fill("_");
+            newWord();
             alert("Congratulations! You win! Play again!");
             winCount = 0;
             guessesRemaining = 15;
-            guessedLetters = [];
         };
     }
-
 
     document.getElementById("winCount").textContent = winCount;
     document.getElementById("guessesRemaining").textContent = guessesRemaining;
     document.getElementById("currentWord").textContent = currentWord.join(" ");
     document.getElementById("guessedLetters").textContent = guessedLetters;
-    console.log(chosenWordValue.indexOf(userGuess));
-
+    document.getElementById("clueText").textContent = chosenWordProperty;
 }
-/*document.getElementById("userGuess").textContent = "Your Guess: "+ userGuess;*/
+
 
 
 
