75ec9909363093fd07649e17805778dea3a87ec3
Nick Ramsay <48420063+nick-ramsay@users.noreply.github.com>
2019-08-13 12:37:41 +1000

    Update to fix nbsp bug inserting space

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -55,7 +55,7 @@ function newWord() {
     currentWord = Array(wordChoices[chosenWordProperty].length).fill("_");
     
     for (i = chosenWordValue.indexOf(" "); i <= chosenWordValue.lastIndexOf(" "); i++) {
-        currentWord[chosenWordValue.indexOf(" ", i)] = "&nbsp;";
+        currentWord[chosenWordValue.indexOf(" ", i)] = &nbsp;
     } //Auto replace _ with a space for object values that contain spaces
 
     chosenWordImageProperty = Object.keys(wordImages)[chosenWordNumber];
