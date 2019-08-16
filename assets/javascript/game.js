76a43b7bc506f1653f8feb5ff51bab6cd233df57
Nick Ramsay <NickRamsay1989@gmail.com>
2019-08-17 09:16:46 +1000

    Added comments to javascript, cleaned up unneccessary code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,7 +21,7 @@ var wordChoices = {
     sweden: "stockholm",
     phillipines: "manilla",
     brazil: "brasilia"
-}
+}; //Object containing countries as properties and capitals as values, provides word used for user guesses
 
 var wordImages = {
     australia: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Australia_with_AAT_%28orthographic_projection%29.svg/375px-Australia_with_AAT_%28orthographic_projection%29.svg.png",
@@ -46,31 +46,34 @@ var wordImages = {
     sweden: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/EU-Sweden_%28orthographic_projection%29.svg/330px-EU-Sweden_%28orthographic_projection%29.svg.png",
     phillipines: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/PHL_orthographic.svg/375px-PHL_orthographic.svg.png",
     brazil: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/BRA_orthographic.svg/375px-BRA_orthographic.svg.png"
-}
+}; //Object using same properties as wordChoices object but values = URL from which to retrieve image for wordChoice clue
 
-var chosenWordImage
+var wins = 0; //Win count, starting at zero
+var losses = 0; //Loss count, starting at zero
 
-var wins = 0;
-var losses = 0;
+var winningScore = 5; //Score at which one is added to win count
+var guessesAllowed = 10; //Number of guesses allowed before one is added to loss count
 
-var winningScore = 5;
-var guessesRemaining = 15;
+var guessesRemaining = 10; //Counter for remaining guesses before loss. Starts at same value as guesses allowed.
 
-var score = 0;
-var guessedLetters;
+var score = 0; //Score count, starting at zero, increasing by 1 each time winningScore is reached
+var guessedLetters = []; //Empty array that will store letters guessed until a win or a loss
 
-var chosenWordHistory = [];
+var chosenWordHistory = []; //Keeps history of words chosen. Array is cleared when all wordChoice properties have been added to it.
 
 var chosenWordNumber; //Generates a random integer equivalent less than or equal to count of properties in the object
 var chosenWordProperty;  //Uses chosenWordNumber to pick a property from object
 var chosenWordValue; //uses chosenWordProperty to pick array value from object
+var chosenWordImage; //Clue image selected using chosenWordProperty
 
-var currentWord;
+var currentWord; //Declaring variable to be used as array storing series of underscores representing un-guessed letters
+
+var message = "Welcome! Press any lowercase letter to start!"; //Message for user. Message is overwritten each time a user selects a key.
 
 function newWord() {
     if (chosenWordHistory.length === Object.keys(wordChoices).length) {
-            chosenWordHistory = [];
-        } // If all the words (properties) in the wordChoice object have been used, clear array to start again    
+        chosenWordHistory = [];
+    } // If all the words (properties) in the wordChoice object have been used, clear array and start a new history   
     for (i = 0; (chosenWordHistory.length === 0 && i === 0) || (chosenWordHistory.length > 0 && chosenWordHistory.indexOf(chosenWordNumber) !== -1); i++) {
         chosenWordNumber = Math.floor(Math.random() * Object.keys(wordChoices).length);
     } //Ensures that the word is only selected once per game.
@@ -81,11 +84,11 @@ function newWord() {
 
     for (i = chosenWordValue.indexOf(" "); i <= chosenWordValue.lastIndexOf(" "); i++) {
         currentWord[chosenWordValue.indexOf(" ", i)] = "&nbsp;";
-    } //Auto replace _ with a space for object values that contain spaces
+    } //Auto replace _ with a space for object values that contain spaces so users don't need to guess
 
-    chosenWordImage = wordImages[chosenWordProperty];
-    guessedLetters = [];
-    chosenWordHistory.push(chosenWordNumber);
+    chosenWordImage = wordImages[chosenWordProperty]; //Uses property randomly picked for wordChoice to pick matching image
+    guessedLetters = []; //Clears history of guessed letters
+    chosenWordHistory.push(chosenWordNumber); //Once number selected to pick a chosen word property, stores number in chosenWordHistory array to not select again.
 
     pageContent();
 }
@@ -100,22 +103,18 @@ function pageContent() {
     document.getElementById("guessedLetters").textContent = guessedLetters.join(" ");
     document.getElementById("clueText").textContent = chosenWordProperty.replace(/_/g, " ");
     document.getElementById("wordImage").src = chosenWordImage;
-}
+    document.getElementById("message").textContent = message;
+} //DOM function responsible for populating all JS output on HTML
 
 document.onkeyup = function (event) {
-    var message;
-    
     var userGuess = event.key;
-
-    var acceptedKeys = "abcdefghijklmnopqrstuvwxyz"
-    //var acceptedKeys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var acceptedKeys = "abcdefghijklmnopqrstuvwxyz";
 
     if (acceptedKeys.indexOf(userGuess) !== -1) {
-        if (guessedLetters.indexOf(userGuess) === -1){
-        guessedLetters.push(userGuess);
-        } //Only push letter to guessedLetter array if it hasn't already been guessed
-        
-        
+        if (guessedLetters.indexOf(userGuess) === -1) {
+            guessedLetters.push(userGuess);
+        } //Only push letter to guessedLetter array if it hasn't already been guessed AND it is in the acceptedKeys array
+
         if (chosenWordValue.indexOf(userGuess) === -1) {
             message = "Oops... wrong letter";
             guessesRemaining--;
@@ -124,22 +123,22 @@ document.onkeyup = function (event) {
                 losses++;
                 newWord();
                 score = 0;
-                guessesRemaining = 15;
+                guessesRemaining = guessesAllowed;
                 guessedLetters = [];
-            }
-        }
+            } //Action to be taken for a loss, where user uses their last remaining guess
+        } //Actions to be taken if user picks a wrong letter
 
         else if (chosenWordValue.indexOf(userGuess) !== -1) {
             message = "Correct!";
             for (i = chosenWordValue.indexOf(userGuess); i <= chosenWordValue.lastIndexOf(userGuess); i++) {
                 currentWord[chosenWordValue.indexOf(userGuess, i)] = userGuess;
-            }
+            } //Action replaces respective "_" in currentWord array with the correctly guessed key value. Repeats loop until all occurences of letter are replaced.
 
             if (currentWord.indexOf("_") === -1 && score < winningScore) {
                 message = "Nicely done! Here's your next country!";
                 score++;
                 newWord();
-            }
+            }//Actions to be taken when user picks correct letter but hasn't reached winningScore
 
             if (score === winningScore) {
                 message = "You win! Play again!";
@@ -148,15 +147,13 @@ document.onkeyup = function (event) {
                 //chosenWordHistory = []; //Clear chosenWordHistory BEFORE selecting new word for next round
                 newWord();
                 score = 0;
-                guessesRemaining = 15;
-            }
-        }
+                guessesRemaining = guessesAllowed;
+            } //Actions taken when user picks correct letter and hits winningScore
+        }//Actions to be taken when user picks a correct letter
     }
 
     else {
         message = "Use lowercase letters only";
-    }
-    document.getElementById("message").textContent = message;
-    pageContent();
+    } //Action to be taken (message only) if user selects an invalid key
+    pageContent(); //Finally, regardless of user's selection, call pageContent() function to output result in HTML
 }
-
